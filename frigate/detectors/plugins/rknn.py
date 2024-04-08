c9152b260265ee740ef48b21f1783f535b61748a
Tony Air <tony@twma.pro>
2024-04-08 17:49:09 +0200

    UPD: librknnrt

--- frigate/detectors/plugins/rknn.py
+++ frigate/detectors/plugins/rknn.py
@@ -1,8 +1,9 @@
 import logging
 import os.path
 import urllib.request
-import numpy as np
 from typing import Literal
+
+import numpy as np
 from pydantic import Field
 
 from frigate.detectors.detection_api import DetectionApi
@@ -17,7 +18,7 @@ supported_socs = ["rk3562", "rk3566", "rk3568", "rk3588"]
 
 class RknnDetectorConfig(BaseDetectorConfig):
     type: Literal[DETECTOR_KEY]
-    core_mask: int = Field(default=0, ge=0, le=7, title="Core mask for NPU.")
+    num_cores: int = Field(default=0, ge=0, le=3, title="Number of NPU cores.")
 
 
 class Rknn(DetectionApi):
@@ -28,7 +29,7 @@ class Rknn(DetectionApi):
         self.width = config.model.width
 
         soc = self.get_soc()
-        core_mask = config.core_mask
+        core_mask = 2**config.num_cores - 1
 
         model_properties = self.get_model_properties(
             config.model.path or "default-yolov8n", soc
